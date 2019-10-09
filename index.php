25881afdbc6a6eaa9cc4feb6f0e5ccbc7166d501
ttbake <ttbake@gmail.com>
2019-10-09 16:33:12 -0400

    Create index.php
    Initial commit of index

---
+++ index.php
@@ -0,0 +1,10 @@
+<title>Todd Baker php</title>
+<h1>Todd Baker php</h1>
+<p>
+<?php
+   echo "Hi there.\n";
+   $answer = 6 * 7;
+   echo "The answer is $answer, what was the question again?\n";
+?>
+</p>
+<p>Yes another paragraph.</p>
